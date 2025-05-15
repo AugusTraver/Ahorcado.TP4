377b8e381b3034d45dd408c961ae1997f3b599b6
TOBIAS ROHR <49549066@ort>
2025-05-15 08:48:42 -0300

    Inicio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Tp4.Models;
+
+namespace Tp4.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
